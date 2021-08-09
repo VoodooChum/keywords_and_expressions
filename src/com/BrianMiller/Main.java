3af2e802c839a29905dacb8f7f41b30fd067441b
brimill <brian.miller.ext@sodexo.com>
2021-08-09 17:10:18 -0500

    Statements

---
+++ src/com/BrianMiller/Main.java
@@ -0,0 +1,14 @@
+package com.BrianMiller;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("This is also " +
+                "a valid statement " +
+                "in Java.");
+    }
+}
